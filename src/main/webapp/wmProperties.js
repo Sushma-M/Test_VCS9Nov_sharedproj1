7d501e52d09420d862ed29840bb3b28e053f9930
sushma maddirala <sushma.maddirala@wavemaker.com>
2018-11-09 09:06:00 +0000

    dde New files added - [test0922,testpage2,testpartial1,AdventureWorks2014,CountryInfoService,FileService,MyJavaService,googleapis,heartRate,hrdb] Files modified - [Common,Login,Main,footer,header,leftnav,rightnav,topnav,securityService,WEB-INF,data,lib,logos,pages,profiles,resources,webapp,.gitignore,.wmproject.properties,README.md,en.json,pom.xml,README.md,Readme.txt,Readme.txt,.wmproject.properties,README.md]

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "TestAllServicesforVCS_31May_PRd2",
+  "homePage" : "Main",
+  "name" : "TestAllServicesforVCS_31May_PRd2",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
